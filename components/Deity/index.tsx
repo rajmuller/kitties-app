1ae458cd9fb4bafcda31c3b9e10a898ac759e44e
adamRajmuller <rajfta@gmail.com>
2022-04-08 16:33:50 +0200

    deus

---
+++ components/Deity/index.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+const Deity = () => {
+  return (
+    <div className="relative h-[500px] w-[500px]">
+      <Image src={} />
+    </div>
+  );
+};
+
+export default Deity;
