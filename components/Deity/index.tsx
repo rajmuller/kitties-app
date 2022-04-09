33cfc491ffa4a37fd2d5290418148500e0047f6c
adamRajmuller <rajfta@gmail.com>
2022-04-09 09:23:59 +0200

    all image

--- components/Deity/index.tsx
+++ components/Deity/index.tsx
@@ -21,19 +21,33 @@ const Deity = ({
 }: DeityProps) => {
   return (
     <div className={`relative h-[500px] w-[500px] ${containerProps}`}>
-      <Image
-        layout="fill"
-        alt="background of Deity"
-        objectFit="cover"
-        src={`/layers/Background/${background}.png`}
-      />
+      {[...new Array(5).map(() => 0)].map((_, index) => {
+        return (
+          <Image
+            key={index}
+            className={
+              index.toString() === background ? "opacity-100" : "opacity-0"
+            }
+            layout="fill"
+            alt="background of Deity"
+            objectFit="cover"
+            src={`/layers/Background/${index + 1}.png`}
+          />
+        );
+      })}
 
-      <Image
-        layout="fill"
-        alt="cape of Deity"
-        objectFit="cover"
-        src={`/layers/Cape/${cape}.png`}
-      />
+      {[...new Array(40).map(() => 0)].map((_, index) => {
+        return (
+          <Image
+            key={index}
+            className={index.toString() === cape ? "opacity-100" : "opacity-0"}
+            layout="fill"
+            alt="cape of Deity"
+            objectFit="cover"
+            src={`/layers/Cape/${index}.png`}
+          />
+        );
+      })}
 
       <Image
         layout="fill"
@@ -42,61 +56,113 @@ const Deity = ({
         src={`/layers/Body/0.png`}
       />
 
-      <Image
-        layout="fill"
-        alt="arm of Deity"
-        objectFit="cover"
-        src={`/layers/Arm/${arm}.png`}
-      />
+      {[...new Array(9).map(() => 0)].map((_, index) => {
+        return (
+          <Image
+            key={index}
+            className={index.toString() === arm ? "opacity-100" : "opacity-0"}
+            layout="fill"
+            alt="arm of Deity"
+            objectFit="cover"
+            src={`/layers/Arm/${index}.png`}
+          />
+        );
+      })}
 
-      <Image
-        layout="fill"
-        alt="eye of Deity"
-        objectFit="cover"
-        src={`/layers/Eye/${eye}.png`}
-      />
+      {[...new Array(9).map(() => 0)].map((_, index) => {
+        return (
+          <Image
+            key={index}
+            className={index.toString() === eye ? "opacity-100" : "opacity-0"}
+            layout="fill"
+            alt="eye of Deity"
+            objectFit="cover"
+            src={`/layers/Eye/${index}.png`}
+          />
+        );
+      })}
 
-      <Image
-        layout="fill"
-        alt="head of Deity"
-        objectFit="cover"
-        src={`/layers/Head/${head}.png`}
-      />
+      {[...new Array(87).map(() => 0)].map((_, index) => {
+        return (
+          <Image
+            key={index}
+            className={index.toString() === head ? "opacity-100" : "opacity-0"}
+            layout="fill"
+            alt="head of Deity"
+            objectFit="cover"
+            src={`/layers/Head/${index}.png`}
+          />
+        );
+      })}
 
-      <Image
-        layout="fill"
-        alt="shoulder of Deity"
-        objectFit="cover"
-        src={`/layers/Shoulder/${shoulder}.png`}
-      />
+      {[...new Array(47).map(() => 0)].map((_, index) => {
+        return (
+          <Image
+            key={index}
+            className={
+              index.toString() === shoulder ? "opacity-100" : "opacity-0"
+            }
+            layout="fill"
+            alt="shoulder of Deity"
+            objectFit="cover"
+            src={`/layers/Shoulder/${index}.png`}
+          />
+        );
+      })}
 
-      <Image
-        layout="fill"
-        alt="chest of Deity"
-        objectFit="cover"
-        src={`/layers/Chest/${chest}.png`}
-      />
+      {[...new Array(40).map(() => 0)].map((_, index) => {
+        return (
+          <Image
+            key={index}
+            className={index.toString() === chest ? "opacity-100" : "opacity-0"}
+            layout="fill"
+            alt="chest of Deity"
+            objectFit="cover"
+            src={`/layers/Chest/${index}.png`}
+          />
+        );
+      })}
 
-      <Image
-        layout="fill"
-        alt="weapon of Deity"
-        objectFit="cover"
-        src={`/layers/Weapon/${weapon}.png`}
-      />
+      {[...new Array(7).map(() => 0)].map((_, index) => {
+        return (
+          <Image
+            key={index}
+            className={
+              index.toString() === weapon ? "opacity-100" : "opacity-0"
+            }
+            layout="fill"
+            alt="weapon of Deity"
+            objectFit="cover"
+            src={`/layers/Weapon/${index}.png`}
+          />
+        );
+      })}
 
-      <Image
-        layout="fill"
-        alt="beak of Deity"
-        objectFit="cover"
-        src={`/layers/Beak/${beak}.png`}
-      />
+      {[...new Array(4).map(() => 0)].map((_, index) => {
+        return (
+          <Image
+            key={index}
+            className={index.toString() === beak ? "opacity-100" : "opacity-0"}
+            layout="fill"
+            alt="beak of Deity"
+            objectFit="cover"
+            src={`/layers/Beak/${index}.png`}
+          />
+        );
+      })}
 
-      <Image
-        layout="fill"
-        alt="face of Deity"
-        objectFit="cover"
-        src={`/layers/Face/${face}.png`}
-      />
+      {[...new Array(64).map(() => 0)].map((_, index) => {
+        return (
+          <Image
+            key={index}
+            className={index.toString() === face ? "opacity-100" : "opacity-0"}
+            layout="fill"
+            alt="face of Deity"
+            objectFit="cover"
+            src={`/layers/Face/${index}.png`}
+          />
+        );
+      })}
     </div>
   );
 };
