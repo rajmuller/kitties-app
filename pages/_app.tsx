ba18eb9199e38f8a149b1dd78ff1fceb716f60b9
adamRajmuller <rajfta@gmail.com>
2022-04-11 17:26:52 +0200

    usedapp config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
 import "@fontsource/mouse-memoirs";
+import { ChainId, Config, DAppProvider } from "@usedapp/core";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import "../styles/cat.css";
 import "../styles/globals.css";
+
+const config: Config = {
+  readOnlyChainId: ChainId.Hardhat,
+  // readOnlyChainId:
+  //   process.env.NEXT_PUBLIC_MUMBAI_ENDPOINT === "local"
+  //     ? ChainId.Hardhat
+  //     : process.env.NEXT_PUBLIC_MUMBAI_ENDPOINT === "testnet"
+  //     ? ChainId.Mumbai
+  //     : ChainId.Polygon,
+  readOnlyUrls: {
+    [ChainId.Hardhat]: "http://127.0.0.1:8545/",
+    [ChainId.Mumbai]: process.env.NEXT_PUBLIC_MUMBAI_ENDPOINT as string,
+    [ChainId.Polygon]: process.env.NEXT_PUBLIC_POLYGON_ENDPOINT as string,
+  },
+};
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
@@ -11,7 +27,10 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
         <title>Kitties</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Component {...pageProps} />;
+
+      <DAppProvider config={config}>
+        <Component {...pageProps} />;
+      </DAppProvider>
     </>
   );
 };
